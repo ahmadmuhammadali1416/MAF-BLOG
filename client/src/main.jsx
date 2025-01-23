06b123ad2035996279fb1fdecf556f3ef30370e6
MAF1-blog <ahmadmuhammadali1416@gmail.com>
2025-01-23 11:22:23 -0800

    all files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import ThemeProvider from "./components/ThemeProvider.jsx";
-
+// files uploaded on github
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <PersistGate persistor={persistor}>
